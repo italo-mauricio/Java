2db2f16133a3b24bcca2de3469314ccd7ff47e97
italo <italogithub@gmail.com>
2022-11-24 15:14:19 -0300

    começando a jornada em java

--- cursojava/src/aula1.java
+++ cursojava/src/aula1.java
@@ -1,7 +1,5 @@
 //              INTRODUÇÃO A LINGUAGEM JAVA
 
-
-
 import java.util.Date;
 
 public class aula1 {
---
+++ cursojava/src/aula2.java
@@ -0,0 +1,11 @@
+
+public class aula2{
+    // armazenar propriedades e variáveis em java
+    public static void main(String[] args){
+        System.out.println(2 + 2);
+        System.out.println(3 * 6);
+        System.out.println("ola mundo!");
+        System.out.println(19 / 3);
+
+    }
+}
