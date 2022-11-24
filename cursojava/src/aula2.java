226a7fa24af71bdadf101993a44896f195c687e6
italo <italogithub@gmail.com>
2022-11-24 15:22:44 -0300

    update

--- cursojava/src/aula2.java
+++ cursojava/src/aula2.java
@@ -6,6 +6,18 @@ public class aula2{
         System.out.println(3 * 6);
         System.out.println("ola mundo!");
         System.out.println(19 / 3);
-
+    
     }
 }
+
+
+/*                      Tópicos de aprendizagem
+ * 
+ * 
+ * pontos de entrada => é o ponto onde todos os sistemas procuram o seu projeto, exemplo do void main
+ * métodos => são instruções que você define para o programa executar, exemplo (System.out.println());
+ * 
+ * 
+ * 
+ * 
+ */
