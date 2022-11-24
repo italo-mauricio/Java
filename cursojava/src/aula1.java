3111ab5922b1d59eebd8a85c7a1889109ade351c
italo <italogithub@gmail.com>
2022-11-24 12:40:53 -0300

    update

---
+++ cursojava/src/aula1.java
@@ -0,0 +1,24 @@
+import java.util.Date;
+
+public class aula1 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("iiio"); 
+        Date data = new Date();
+        System.out.println(data);
+    }
+    
+}
+
+
+// sout = reproduz autoameticamente o system out print
+// psvm = public static void main (gera automaticamente)
+// camelcase = técnica que exibe os nomes das classes em formas de camelos "Mina Classe"
+// primeira letra maiuscula é sempre uma classe
+/*
+ * Se a primeira letra for minúscula e aparecer uma letra maiuscula após ela pode ser:
+ * Atributo
+ * Variavel
+ * Método
+ * meu_pacote - se for tudo em letra minuscula
+ * 
+ */
