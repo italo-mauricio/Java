40d6951b9d866b24cca4c66666a8e69577743937
italo <italogithub@gmail.com>
2022-11-25 05:37:11 -0300

    args

--- cursojava/src/aula4.java
+++ cursojava/src/aula4.java
@@ -1,7 +1,7 @@
 public class aula4{
 
     public static void main(String[] args) {
-        sum( 10, 20);
+        sum( 10, 20);  // argumentos
         sum( 15, 20);
         sum( 1223, 20);
         sum( 133, 2022);
@@ -10,6 +10,7 @@ public class aula4{
     static void sum(int x, int y) {
         // static void = declaração padrão
         // sum = nome da função, pode ser qualquer nome
+        // () = usamos para definir os parâmetros da função
 
 
         System.out.println(x + y);  // o método pode ser reutilizado em outras partes do código
---
+++ cursojava/src/aula5.java
@@ -0,0 +1,19 @@
+public class aula5 {
+
+    public static void main(String[] args) {
+        System.out.println("ola " + args[0]);
+        System.out.println("ola " + args[1]);
+        System.out.println("ola " + args[2]);
+        System.out.println("ola " + args[3]);
+    }
+    
+}
+
+/*
+ * Basicamente no java, trabalhamos com o args como argumentos
+ * com esse método, podemos definir no inicio da função quais parametros são lidos através do programa
+ * 
+ * 
+ * 
+ * 
+ */
