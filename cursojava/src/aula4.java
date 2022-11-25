9c0354468453e7dcb0373b1d42134de0b58d0b74
italo <italogithub@gmail.com>
2022-11-25 05:24:54 -0300

    update

--- cursojava/src/aula4.java
+++ cursojava/src/aula4.java
@@ -1,11 +1,18 @@
 public class aula4{
 
     public static void main(String[] args) {
-        
+        sum( 10, 20);
+        sum( 15, 20);
+        sum( 1223, 20);
+        sum( 133, 2022);
     }
 
     static void sum(int x, int y) {
-        
+        // static void = declaração padrão
+        // sum = nome da função, pode ser qualquer nome
+
+
+        System.out.println(x + y);  // o método pode ser reutilizado em outras partes do código
 
     }
 
