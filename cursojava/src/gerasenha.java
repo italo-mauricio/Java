79283f33b716e23fb44eb6fc23e4bb09e116a16e
italo <italogithub@gmail.com>
2022-11-26 19:30:26 -0300

    update

--- cursojava/src/gerasenha.java
+++ cursojava/src/gerasenha.java
@@ -3,7 +3,7 @@ import java.util.Random;
 public class gerasenha{
 
 
-    // teste1
+    // teste11
     public static void main(String[] args) {
         Random gerador = new Random();
         // utilizando o while
