a56a720c3c08cf8d3f08835ea674bf74f006b0d6
italo <italogithub@gmail.com>
2022-11-26 13:18:06 -0300

    utilizando os dois laços

--- cursojava/src/gerasenha.java
+++ cursojava/src/gerasenha.java
@@ -4,12 +4,19 @@ public class gerasenha{
     
     public static void main(String[] args) {
         Random gerador = new Random();
-
-        int i = 0;
-        while(i < 6){
+        // utilizando o while
+       int a = 0;
+        while(a < 6){
             int number = gerador.nextInt(60);
             System.out.println(number);
-            i+=1;
+         a+=1;
+
+        }
+       // utilizando o for
+        for(int i = 0; i < 6; i++){
+            int number = gerador.nextInt(60);
+            System.out.println(number);
+
 
         }
     }
