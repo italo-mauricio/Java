763da8d27365fdcc67c708c2d14d26d86c3ab608
italo <italogithub@gmail.com>
2022-11-27 15:10:49 -0300

    update

--- cursojava/src/Aula7.java
+++ cursojava/src/Aula7.java
@@ -1,10 +1,13 @@
 public class Aula7 {
     public static void main(String[] args) {
         // escopo de variável
+        int x;
         if (true){
-            int x = 19;   // variável local, declaraada dentro desse escopo
+            x = 19;   // variável local, declaraada dentro desse escopo
+        } else{
+            x = 2;
         }
-        int x = 20;  // só consigo fazer a chamada da variável se ela for declarada fora do escopo
+      // só consigo fazer a chamada da variável se ela for declarada fora do escopo
         System.out.println(x);
 
     }
