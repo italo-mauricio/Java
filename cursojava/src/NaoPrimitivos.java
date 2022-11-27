3bb2fee0d5becddfc5824a78db55fa417770aa39
italo <italogithub@gmail.com>
2022-11-27 14:29:52 -0300

    não primitivos

--- cursojava/src/NaoPrimitivos.java
+++ cursojava/src/NaoPrimitivos.java
@@ -4,9 +4,11 @@
 public class NaoPrimitivos {
 
     public static void main(String[] args) {
-        int x = 2;
-        Integer y = 4;
+        int x = 2;  // primitivo
+        Integer y = 4; // não primitivo
         System.out.println(x);
         System.out.println(y);
     }
 }
+
+// dê preferencia aos primitivos para armazenar dados
