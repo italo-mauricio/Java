a03234f55c9df2d4a9924c4cea97f453a255470d
italo <italogithub@gmail.com>
2022-11-26 21:56:17 -0300

    entrada de dados

--- cursojava/src/User.java
+++ cursojava/src/User.java
@@ -3,12 +3,13 @@ import java.util.Scanner;
 // trabalhando com entrada de dados
 public class User {
     public static void main(String[] args) {
-
+        // exemplo de entrada de datdos
         Scanner scanner = new Scanner(System.in);
-
+        System.out.println("Digite o seu nome: ");
         String name = scanner.nextLine();
+        System.out.println("Ola: " + name);
 
-        System.out.println(name);
+
 
     }
 }
