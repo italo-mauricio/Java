5f91f4433950f18d28782de61125e8a9352e64e6
italo <italogithub@gmail.com>
2022-11-28 10:29:19 -0300

    update

--- cursojava/src/com/italomauricio/user/Visibilidade.java
+++ cursojava/src/com/italomauricio/user/Visibilidade.java
@@ -2,10 +2,19 @@ package com.italomauricio.user;
 
 public class Visibilidade {
 
+    // public (visibilidade publica para todos os packages
      public static int x;
+
+    // private (visibilidade privada para todos os packages, somente a classe atual pode acessar
+     private static int y;
+
+    // depende dos conceitos de orientação a objetos
+     protected static int v;
 
     public static void main(String[] args) {
         // Public = conceito de visibilidade de escopo
+
+
     }
 }
 // com o método public eu consigo deixar publico a declaração para qualquer usuario do pacote.
