1b9245470fdbcd8b7ff3d8a66956e721dd19c443
italo <italogithub@gmail.com>
2022-11-28 11:17:10 -0300

    tipos primitivos

--- cursojava/src/Aulas/aula6.java
+++ cursojava/src/Aulas/aula6.java
@@ -1,11 +1,21 @@
 package Aulas;
 
-import java.util.Random;
-
-
 public class aula6 {
 
     public static void main(String[] args) {
+        // tipos primitivos
+
+        boolean isLooged = true;
+        byte b = 'a';
+        char a = 'c';
+        String character = "c";
+        short s = -129;  // = int só que com menos espaços para alocar números
+        int i = 213213123;
+        long l = 12321321231324L; // precisa ter o L maiusculo no final
+        float f = 10.232f;  // 32 bits
+        double d = 19.232;  // 64 bits
+
+
         
     }
     
