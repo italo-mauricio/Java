6facf4e20b78f22f9944bc4c80d9318583149fc8
italo <italogithub@gmail.com>
2022-11-28 15:15:54 -0300

    update

--- cursojava/src/Aulas/Aula10.java
+++ cursojava/src/Aulas/Aula10.java
@@ -12,5 +12,13 @@ public class Aula10 {
         String pass = scanner.nextLine();
 
         System.out.println(password.equals(pass));
+
+        // o == não compara o conteudo, mas sim, a referência dos objetos
+        //System.out.println(password == pass);
+
+        // usando new String a comparação dará false, pois estão armazenados em endereços diferentes
+        String x = new String( "123456");
+        String y = new String( "123456");
+        System.out.println(x == y);
     }
 }
