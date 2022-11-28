52d9594644993a798b951f8818db259efa4ce6f4
italo <italogithub@gmail.com>
2022-11-28 11:42:58 -0300

    conversões

---
+++ cursojava/src/Aulas/Aula8.java
@@ -0,0 +1,13 @@
+package Aulas;
+
+public class Aula8 {
+    public static void main(String[] args) {
+        // converter tipos primitivos
+
+        double dd = 10.10;
+        short ss = 32;
+
+        float x = (float) dd / ss;    // dessa forma, conseguimos fazer a conversão para o tipo float
+        System.out.println(dd / ss);
+    }
+}
