84404c28db2b7b89d3a4a3208c38dec55b01a743
italo <italogithub@gmail.com>
2022-11-28 11:56:24 -0300

    update

--- cursojava/src/Aulas/Aula8.java
+++ cursojava/src/Aulas/Aula8.java
@@ -1,5 +1,7 @@
 package Aulas;
 
+import java.util.Scanner;
+
 public class Aula8 {
     public static void main(String[] args) {
         // converter tipos primitivos
@@ -8,6 +10,14 @@ public class Aula8 {
         short ss = 32;
 
         float x = (float) dd / ss;    // dessa forma, conseguimos fazer a conversão para o tipo float
-        System.out.println(dd / ss);
+        int y = (int) dd / ss;
+
+        Scanner scanner = new Scanner(System.in);
+        int z = (int) scanner.nextDouble();
+
+        System.out.println(z);
+
+        System.out.println(x);
+        System.out.println(y);
     }
 }
