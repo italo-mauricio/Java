a13e338b0707111a43ceb73b15466d002eb43fc2
italo <italogithub@gmail.com>
2022-11-28 12:24:16 -0300

    manipulando strings

--- cursojava/src/Aulas/Aula9.java
+++ cursojava/src/Aulas/Aula9.java
@@ -8,5 +8,11 @@ public class Aula9 {
         System.out.println(x + "concatenada");  // concatena
         System.out.println(x.contains("italo"));; // verifica se a palavra existe na frase
         System.out.println(x.indexOf("italo")); // indica em qual caractere começa a palavra
+        System.out.println(x.lastIndexOf("italo")); // indica a ultima ocorrencia
+        System.out.println(x.toUpperCase());  // deixa tudo em maiúsculo
+        System.out.println(x.toLowerCase());  // deixa tudo em minúsculo
+        System.out.println(x.trim());  // remove os espaços
+        System.out.println(x.substring(9));  // busca a frase a partir do caractere desejado
+        System.out.println(x.equals("ola"));   // comparação
     }
 }
