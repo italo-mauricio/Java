323009d712a053742cd7714f53ecad76bc2aec0a
italo <italogithub@gmail.com>
2022-11-29 08:31:40 -0300

    introdução a POO

---
+++ cursojava/src/POO/Intro.java
@@ -0,0 +1,9 @@
+package POO;
+
+public class Intro {
+    public static void main(String[] args) {
+        User userA = new User();
+        userA.firstName = "Italo";
+        userA.lastName = "Mauricio";
+    }
+}
---
+++ cursojava/src/POO/User.java
@@ -0,0 +1,10 @@
+package POO;
+
+public class User {
+
+    // membros = proriedades e métodos que são os comportamentes que essa classe vai ter
+
+    public String firstName;
+    public String lastName;
+
+}
