a4b909e58d6c267d744d3325c4de5999d2e7f5de
italo <italogithub@gmail.com>
2022-11-29 08:45:07 -0300

    update

---
+++ cursojava/src/POO/Guitar.java
@@ -0,0 +1,14 @@
+package POO;
+
+public class Guitar {
+
+    // propriedades
+    public String name;
+    public int chords;
+
+    // comportamento
+    public void play(){
+        // aqui vai ser tocado o audio
+    }
+
+}
--- cursojava/src/POO/Intro.java
+++ cursojava/src/POO/Intro.java
@@ -2,8 +2,13 @@ package POO;
 
 public class Intro {
     public static void main(String[] args) {
-        User userA = new User();
-        userA.firstName = "Italo";
-        userA.lastName = "Mauricio";
+        Guitar fender = new Guitar();
+        fender.chords = 6;
+        fender.name = "Fender";
+
+        Guitar ibanez = new Guitar();
+        ibanez.chords = 6;
+        ibanez.name = "Ibanez";
+
     }
 }
