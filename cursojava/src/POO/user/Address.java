ace23cbfeeabe48c7e72dc5fdd1255e7d8d03ecc
italo <italogithub@gmail.com>
2022-11-29 09:06:52 -0300

    update

---
+++ cursojava/src/POO/user/Address.java
@@ -0,0 +1,9 @@
+package POO.user;
+
+public class Address {
+    public String street;
+    public String zipCode;
+    public String number;
+}
+
+//exemplos de modularização na criação de métodos e comportamentos
--- cursojava/src/POO/user/User.java
+++ cursojava/src/POO/user/User.java
@@ -2,13 +2,14 @@ package POO.user;
 
 
 public class User {
-    private boolean islogged;
     public String firstName;
     public String lastName;
     public String fullName;
+    public Address address;
 
     public String getFullName(){
-        return firstName + " " + lastName;
+        fullName =  firstName + " " + lastName;
+        return fullName;
     }
 
 
